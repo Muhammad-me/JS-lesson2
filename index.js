9bf0f06f44bd45af80c35002e00ea8f2cf0014d4
Muhammad-me <mmajidhanov@gmail.com>
2022-06-19 11:50:10 +0500

    Mening JSdagi 2-darsim

---
+++ index.js
@@ -0,0 +1,50 @@
+// Truthy Falsy
+//Falsy = [0, "", false, null, undefined, NaN]
+//Truthy = [0 dan katta har qanday son, bo'sh bo'lmagan har qanday string]
+
+
+// let ism = ("Muhammad");
+// let foydalanuvchilar = ["Jakhongir", "Murodjon", "Boburjon", "Azimjon" ,"Akmalxon", "Farruhbek", "Kamoldin", ism, "MuhammadYusuf", "Vorisxon", "Hikmatillo"]
+// console.log(`Welcome ${foydalanuvchilar[0]}, ${foydalanuvchilar[1]} to my site!`);
+// if (5){
+//     console.log("Hello");
+
+
+// if (12 > "5"){
+//     console.log((3 < 6 < 9));
+//     console.log((3 > 6 > 9));
+// }
+
+let ism = prompt("Hello. Please write your name");
+if (ism == ""){
+    alert("  You must enter your name");
+}else{
+    alert("Welcome to site!")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
